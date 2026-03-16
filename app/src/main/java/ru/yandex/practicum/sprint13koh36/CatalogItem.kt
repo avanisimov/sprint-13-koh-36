be6a973025cf2564a1d75c7812ff9a22bbdb6200
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2026-03-16 17:40:13 +0300

    Init commit

---
+++ app/src/main/java/ru/yandex/practicum/sprint13koh36/CatalogItem.kt
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.sprint13koh36
+
+data class CatalogItem(
+    val id: String,
+    val imageUrl: String,
+    val name: String,
+    val price: Long,
+    val unit: String,
+)
